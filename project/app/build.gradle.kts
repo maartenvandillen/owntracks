10c4b21e1e712f18deb1c599f3cfce6b2d519527
Maarten van Dillen <mvandillen@gmail.com>
2025-05-14 11:27:00 +0200

    release 3.0.2

--- project/app/build.gradle.kts
+++ project/app/build.gradle.kts
@@ -22,7 +22,7 @@ val googleMapsAPIKey =
 val gmsImplementation: Configuration by configurations.creating
 
 //val packageVersionCode: Int = System.getenv("VERSION_CODE")?.toInt() ?: 420503000
-val packageVersionCode: Int = 4
+val packageVersionCode: Int = 5
 val manuallySetVersion: Boolean = System.getenv("VERSION_CODE") != null
 
 android {
@@ -35,7 +35,7 @@ android {
     targetSdk = 34
 
     versionCode = packageVersionCode
-    versionName = "3.0.1"
+    versionName = "3.0.2"
 
     val localeCount = fileTree("src/main/res/").map {
       it.toPath()
